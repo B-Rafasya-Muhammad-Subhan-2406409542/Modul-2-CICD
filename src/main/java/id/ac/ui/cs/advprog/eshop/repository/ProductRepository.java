822b11a08620e926e5ea52be5c8591c1d45ba7d0
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-02-06 17:44:38 +0700

    feat: implement UUID and edit logic in repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,17 +6,42 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null) {
+            product.setProductId(UUID.randomUUID().toString());
+        }
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Product findById(String id) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product update(Product product) {
+        for (int i = 0; i < productData.size(); i++) {
+            Product currentProduct = productData.get(i);
+            if (currentProduct.getProductId().equals(product.getProductId())) {
+                currentProduct.setProductName(product.getProductName());
+                currentProduct.setProductQuantity(product.getProductQuantity());
+                return currentProduct;
+            }
+        }
+        return null;
     }
 }
